713b7b5e062f19fe9375491548bfcb0c4ccabf17
Kostiantyn Polishko <167745085+KostiantynPolishko2@users.noreply.github.com>
2024-08-27 12:12:35 +0300

    1 Done AuthenticateController for test WeatherForecastController (#1)

---
+++ SQL/AuthData.sql
@@ -0,0 +1,15 @@
+USE [authdata]
+
+SELECT * FROM [dbo].[AspNetRoleClaims];
+
+SELECT * FROM [dbo].[AspNetRoles];
+
+SELECT * FROM [dbo].[AspNetUserClaims];
+
+SELECT * FROM [dbo].[AspNetUserLogins];
+
+SELECT * FROM [dbo].[AspNetUserRoles];
+
+SELECT * FROM [dbo].[AspNetUsers];
+
+SELECT * FROM [dbo].[AspNetUserTokens];
