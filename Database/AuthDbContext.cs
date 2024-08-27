a03f8efd88e44068a3bb55f022369e8a328a907e
Kostiantyn Polishko <167745085+KostiantynPolishko2@users.noreply.github.com>
2024-08-27 12:14:12 +0300

    1 Done AuthenticateController for test WeatherForecastController (#1) (#2)

---
+++ Database/AuthDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Drawing;
+
+namespace AuthJWTAspNetWeb.Database
+{
+    namespace _20240723_SqlDb_Gai.Database
+    {
+        public class AuthDbContext : IdentityDbContext<IdentityUser>
+        {
+            public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options) { }
+
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                base.OnModelCreating(modelBuilder);
+            }
+        }
+    }
+}
